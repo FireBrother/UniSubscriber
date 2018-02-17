2c19688e0ea3373241bc640ab55072f79788a731
Xian Wu <wuxian94@pku.edu.cn>
2018-02-17 10:46:57 +0800

    qq垃圾邮件

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from Monitors.ThreeDMMonitor import ThreeDMMonitor
 
 if __name__ == '__main__':
     tdm = ThreeDMMonitor('闪轨2汉化帖', 'http://bbs.3dmgame.com/thread-5703118-1-1.html')
-    ma = MailAnnouncer('ma', 'smtp.qq.com', '417462933@qq.com', getpass(), ['417462933@qq.com'])
+    ma1 = MailAnnouncer('ma1', 'smtp.qq.com', '417462933@qq.com', '7766Andy', ['417462933@qq.com'])
+    ma2 = MailAnnouncer('ma2', 'smtp.qq.com', '417462933@qq.com', '7766Andy', ['yangjiachen.pku@gmail.com'])
     master = SubscriberMaster('sm1')
 
-    master.add_announcer(ma)
+    master.add_announcer(ma1)
+    master.add_announcer(ma2)
     master.add_monitor(tdm)
     master.start()
     master.join()
